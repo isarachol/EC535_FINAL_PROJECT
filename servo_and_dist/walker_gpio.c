74c525280eba180f3d13d2c289d8807aad204ca6
Isara Cholaseuk <tand.isara2001@gmail.com>
2025-12-05 03:02:29 -0500

    Servo + distance working now

--- servo_and_dist/walker_gpio.c
+++ servo_and_dist/walker_gpio.c
@@ -10,53 +10,66 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/device.h>
+#include <linux/pwm.h>
+
 
 MODULE_LICENSE("GPL");
 
 
+// ============================================
+// DEFINITIONS and constants
+// ============================================
 //Define pins
 #define trig_pin 47 // Don't use 117, always HIGH
 #define echo_pin 27
-// #define pwmb_pin 66 // back
-// #define pwml_pin 69 // left
-// #define pwmr_pin 45 // right
 #define btn0_pin 26
 #define btn1_pin 46
-
+#define pwm0_id 1 // back
+#define pwm1_id 4 // left
+#define pwm2_id 5 // right
 
 //Device name
 #define DEVICE_NAME "walker"
 #define major_number 61
 #define minor_number 0
 
-
 // Define constants
 static const unsigned long trig_usec = 10; // send 10 usec pulse
 static const unsigned long sound_speed = 343; // m/s
-static const unsigned long stop_distance = 10; // cm
+static const unsigned long dist_goal = 10; // cm
 static const unsigned long cal_trig = 10; // calibration bias
+static const int pwm_period = 20000000; // 20ms in ns
+static const int duty_center = 1500000; // 1.5ms
+static const int duty_span = 900000; // +-0.9ms
 
 
-//Global Variable 
-static struct gpio_desc *trig, *echo, *btn0, *btn1; //*trig, *green, *yellow, *btn0, *btn1;
+// ============================================
+// Global Variable s
+// ============================================
+// GPIO
+static struct gpio_desc *trig, *echo, *btn0, *btn1;
 static ktime_t start_trig, end_trig, elapsed_trig; 
-static unsigned long dist_um = 1000000000;
+static unsigned long dist_cm = 1000000000;
 static int echoed = 1;
 static int irq_echo = 0; // for receiving trig signal
 static int irq_btn0 = 0; //for changing mode
 static int irq_btn1 = 0; //for ptrigstrain 
+// PWM
+static struct pwm_device *pwm0, *pwm1, *pwm2; // back, left, right
+static bool pwm0_enabled, pwm1_enabled, pwm2_enabled;
+static int pwm0_state, pwm1_state, pwm2_state; // state = -10, 0, 10
+// Timer
 static struct timer_list dist_timer; //for tracking cycle
 
 //Variable for printing
 static int current_mode = 0; // 0=stop, 1 = forward, 2=turn-left, 3=turn-right
 static int trig_rate = 1; // default = 1 Hz
 static int count_cycle = 0; //Keep track of cycle
-// static bool pedrestrian_press = false; //For track at the start of trig
-// static bool pedrestrian_cross = false; //For flashing yellow 5 cycles
-// static bool lightbulb_checked = false; //For after checking lightbulb
 
 
-//Function
+// ============================================
+// Function prototype
+// ============================================
 static int dev_open(struct inode *inode, struct file *filp);
 static int dev_release(struct inode *inode, struct file *filp);
 static ssize_t dev_read(struct file *filp,
@@ -72,8 +85,14 @@ static int walker_init (void);
 static void walker_exit (void);
 static void timer_callback(struct timer_list *t);
 static int walker_setup (void);
+// Distance sensor
+static irqreturn_t echo_handler(int irq, void *dev_id);
 static void blink_trig(void);
 static unsigned long calc_dist_cm(unsigned long ns);
+// PWM
+static int calc_duty(int state);
+static void move(void);
+static void actuate(void);
 
 
 //Operation
@@ -85,29 +104,56 @@ static struct file_operations walker_fops = {
 };
 
 
-/* Function to init and exit functions */
-module_init(walker_init);
-module_exit(walker_exit);
+// ============================================
+// Function Definitions
+// ============================================
 
-// Helper function
-
-static void blink_trig() {
-	gpiod_set_value(trig, 1);
-	udelay(trig_usec);
-	gpiod_set_value(trig, 0);
-	start_trig = ktime_get_ns();
-}
-
-static unsigned long calc_dist_cm(unsigned long ns) {
-	return ns * sound_speed / 10000000 / 2 - cal_trig; // forth and back
-}
-
-//Callback function 
+// Callback function 
 static void timer_callback(struct timer_list *t){
+
 	if (echoed == 0)
 		printk(KERN_ALERT "Echo not heard! Try again\n");
 	echoed = 0;
 	blink_trig();
+
+    // pwm_config(pwm0, calc_duty(((count_cycle + 0) % 3) - 1), pwm_period);
+    // pwm_config(pwm1, calc_duty(((count_cycle + 1) % 3) - 1), pwm_period);
+    // pwm_config(pwm2, calc_duty(((count_cycle + 2) % 3) - 1), pwm_period);
+    if (!pwm0_enabled) {
+        pwm_enable(pwm0);
+    }
+    if (!pwm1_enabled) {
+        pwm_enable(pwm1);
+    }
+    if (!pwm2_enabled) {
+        pwm_enable(pwm2);
+    }
+
+    // goal reached
+    if (dist_cm <= dist_goal) {
+        current_mode = 0;
+        pwm0_enabled = false;
+        pwm1_enabled = false;
+        pwm2_enabled = false;
+        pwm_disable(pwm0);
+        pwm_disable(pwm1);
+        pwm_disable(pwm2);
+        pr_info("Goal reached!\n");
+    }
+    else {
+        pwm0_enabled = true;
+        pwm1_enabled = true;
+        pwm2_enabled = true;
+        pwm_enable(pwm0);
+        pwm_enable(pwm1);
+        pwm_enable(pwm2);
+
+        // TODO determine mode
+        current_mode = 1;
+        if (current_mode == 1) {
+            move();
+        }
+    }
 
 	count_cycle++;
 	mod_timer(&dist_timer, jiffies + msecs_to_jiffies(1000 / trig_rate));
@@ -152,13 +198,7 @@ static int walker_setup (void){
     if (ret) { gpio_free(trig_pin); gpio_free(echo_pin); return ret; }
     ret = gpio_request(btn1_pin, "btn1");
     if (ret) { gpio_free(trig_pin); gpio_free(echo_pin); gpio_free(btn0_pin); return ret; }
-	// ret = gpio_request(pwmb_pin, "btn1");
-    // if (ret) { gpio_free(trig_pin); gpio_free(echo_pin); gpio_free(btn0_pin); gpio_free(btn1_pin); return ret; }
-	// ret = gpio_request(pwml_pin, "btn1");
-    // if (ret) { gpio_free(trig_pin); gpio_free(echo_pin); gpio_free(btn0_pin); gpio_free(btn1_pin); gpio_free(pwmb_pin); return ret; }
-	// ret = gpio_request(pwmr_pin, "btn1");
-    // if (ret) { gpio_free(trig_pin); gpio_free(echo_pin); gpio_free(btn0_pin); gpio_free(btn1_pin); gpio_free(pwmb_pin); gpio_free(pwml_pin); return ret; }
-	
+		
 
     // Convert legacy to desc
     trig = setup_gpio(trig_pin); 
@@ -170,24 +210,11 @@ static int walker_setup (void){
     btn1 = setup_gpio(btn1_pin);
     if(IS_ERR(btn1)) return(PTR_ERR(btn1));
 
-    // pwmb = setup_gpio(pwmb_pin);
-    // if(IS_ERR(pwmb)) return(PTR_ERR(pwmb));
-    // pwml = setup_gpio(pwml_pin);
-    // if(IS_ERR(pwml)) return(PTR_ERR(pwml));
-    // pwmr = setup_gpio(pwmr_pin);
-    // if(IS_ERR(pwmr)) return(PTR_ERR(pwmr));
-
+    
     /*========= Setup output and input =========*/
     // out
     ret = gpio_direction(trig, true, 0);
     if (ret) return ret;
-	// ret = gpio_direction(pwmb, true, 0);
-    // if (ret) return ret;
-	// ret = gpio_direction(pwml, true, 0);
-    // if (ret) return ret;
-	// ret = gpio_direction(pwmr, true, 0);
-    // if (ret) return ret;
-	// in
     ret = gpio_direction(echo, false, 0);
     if (ret) return ret;
     ret = gpio_direction(btn0, false, 0); 
@@ -202,28 +229,68 @@ static int walker_setup (void){
 static void free_gpio_pins(void){
     // Turn off all outputs
     if (!IS_ERR_OR_NULL(trig)) gpiod_set_value(trig, 0);
-	// if (!IS_ERR_OR_NULL(pwmb)) gpiod_set_value(pwmb, 0);
-	// if (!IS_ERR_OR_NULL(pwml)) gpiod_set_value(pwml, 0);
-	// if (!IS_ERR_OR_NULL(pwmr)) gpiod_set_value(pwmr, 0);
     
     // Release all GPIOs (CHANGED - use gpio_free)
 	gpio_free(trig_pin);
 	gpio_free(echo_pin);
     gpio_free(btn0_pin);
     gpio_free(btn1_pin);
-    // gpio_free(pwmb_pin);
-    // gpio_free(pwml_pin);
-    // gpio_free(pwmr_pin);
 }
 
-// Echo heard
+// PWM helper functions
+// state in range [-10 , 10]
+static int calc_duty(int state){
+    int duty = duty_center + state * duty_span / 10;
+    return duty;
+}
+
+static void actuate(void) {
+    pwm_config(pwm0, calc_duty(pwm0_state), pwm_period);
+    pwm_config(pwm1, calc_duty(pwm1_state), pwm_period);
+    pwm_config(pwm2, calc_duty(pwm2_state), pwm_period);
+}
+
+// direction determined by pwm0_state in range [-10 , 10] ========== need assumption validation =============
+static void move(void){
+
+    // if not parallel, or not fully extended
+    if (pwm1_state != -pwm2_state || (pwm1_state != 10 && pwm1_state != -10)) {
+        // prepare to push forward
+        pwm1_state = -10;
+        pwm2_state = 10;
+    }
+    else { // if prepared, update state
+        pwm1_state = -pwm1_state;
+        pwm2_state = -pwm2_state;
+    }
+
+    // steer 0 if forward, 5 if right, -5 if left --> need range of turning angle
+    if (pwm0_state < -5) pwm0_state = -5;
+    else if (pwm0_state > 5) pwm0_state = 5;
+    pwm0_state = calc_duty(pwm0_state);
+
+    actuate();
+}
+
+// Echo helper function
+static void blink_trig(void) {
+	gpiod_set_value(trig, 1);
+	udelay(trig_usec);
+	gpiod_set_value(trig, 0);
+	start_trig = ktime_get_ns();
+}
+
+static unsigned long calc_dist_cm(unsigned long ns) {
+	return ns * sound_speed / 10000000 / 2 - cal_trig; // forth and back
+}
+
 static irqreturn_t echo_handler(int irq, void *dev_id)
 {
 	end_trig = ktime_get_ns();
 	elapsed_trig = end_trig - start_trig;
 
 	echoed = 1;
-	dist_um = calc_dist_cm((unsigned long)elapsed_trig);
+	dist_cm = calc_dist_cm((unsigned long)elapsed_trig);
     return IRQ_HANDLED;
 }
 
@@ -240,6 +307,11 @@ static irqreturn_t btn1_handler(int irq, void *dev_id)
     // TODO
     return IRQ_HANDLED;
 }
+
+
+// ============================================
+// Character Device Functions
+// ============================================
 
 static ssize_t dev_read(struct file *filp, char *buf, size_t count, loff_t *f_pos){
     int msg_len;
@@ -268,7 +340,7 @@ static ssize_t dev_read(struct file *filp, char *buf, size_t count, loff_t *f_po
     msg_len = snprintf(msg, sizeof(msg), 
                 "\nCurrent Mode: %s\n"
                 "Trig Rate: %d Hz\n"
-                "distance (cm): %lu \n\n", mode, trig_rate, dist_um); // convert to cm
+                "distance (cm): %lu \n\n", mode, trig_rate, dist_cm); // convert to cm
 
     // Only read once
     if (*f_pos > 0){
@@ -391,6 +463,47 @@ static int walker_init(void)
         return ret;
     }
 
+	// Request pwm
+	pwm0 = pwm_request(1, "pwm0");
+	if (pwm0 == NULL) {
+        pr_info("pwm0 failed\n");
+        free_irq(irq_echo, NULL);
+        free_irq(irq_btn0, NULL);
+        free_irq(irq_btn1, NULL);
+        free_gpio_pins();
+        unregister_chrdev(major_number, "walker");
+    }
+    pwm_config(pwm0, duty_center, pwm_period);
+    pwm0_enabled = false;
+    pwm0_state = -10;
+
+    pwm1 = pwm_request(4, "pwm0");
+	if (pwm1 == NULL) {
+        pr_info("pwm1 failed\n");
+        free_irq(irq_echo, NULL);
+        free_irq(irq_btn0, NULL);
+        free_irq(irq_btn1, NULL);
+        pwm_free(pwm0);
+    }
+    pwm_config(pwm1, duty_center, pwm_period);
+    pwm1_enabled = false;
+    pwm1_state = -10;
+
+    pwm2 = pwm_request(5, "pwm0");
+	if (pwm2 == NULL) {
+        pr_info("pwm2 failed\n");
+        free_irq(irq_echo, NULL);
+        free_irq(irq_btn0, NULL);
+        free_irq(irq_btn1, NULL);
+        pwm_free(pwm0);
+        pwm_free(pwm1);
+        free_gpio_pins();
+        unregister_chrdev(major_number, "walker");
+    }
+    pwm_config(pwm2, duty_center, pwm_period);
+    pwm2_enabled = false;
+    pwm2_state = -10;
+
 	// Initialize and start timer
     timer_setup(&dist_timer, timer_callback, 0);
     mod_timer(&dist_timer, jiffies + msecs_to_jiffies(1000 / trig_rate));
@@ -401,12 +514,21 @@ static int walker_init(void)
 static void walker_exit(void)
 {
     // Delete timer
-    del_timer_sync(&dist_timer);
+	if (timer_pending(&dist_timer))
+    	del_timer_sync(&dist_timer);
 
     // Free IRQs (ADDED - REQUItrig!)
 	if (irq_echo > 0) free_irq(irq_echo, NULL);
     if (irq_btn0 > 0) free_irq(irq_btn0, NULL);
     if (irq_btn1 > 0) free_irq(irq_btn1, NULL);
+
+	// Free pwm
+    if (pwm0_enabled) pwm_disable(pwm0);
+    if (pwm1_enabled) pwm_disable(pwm1);
+    if (pwm2_enabled) pwm_disable(pwm2);
+    pwm_free(pwm0);
+    pwm_free(pwm1);
+    pwm_free(pwm2);
 
     // Free GPIO pins
     free_gpio_pins();
@@ -416,3 +538,7 @@ static void walker_exit(void)
 
     printk(KERN_ALERT "Removing walker module\n");
 }
+
+/* Function to init and exit functions */
+module_init(walker_init);
+module_exit(walker_exit);
